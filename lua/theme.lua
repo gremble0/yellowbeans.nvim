ab81528bcfac658b5066364c5bc43334b730b77f
gremble0 <hermanstornes@gmail.com>
2023-07-14 07:19:02 +0200

    Added palette entry to M table and added more links

--- lua/theme.lua
+++ lua/theme.lua
@@ -4,189 +4,197 @@ end
 
 local M = {}
 
-local bg = "#151515"
-local fg = "#cccccc"
-local black_one = "#101010"
-local black_two = "#191919"
-local black_three = "#1c1c1c"
-local white_one = "#aaaaaa"
-local gray_one = "#333333"
-local gray_two = "#606060"
+M.palette = {
+  bg = "#151515",
+  fg = "#cccccc",
 
-local gold_yellow = "#e1b655"
-local koromiko_yellow = "#ffb964"
-local olive_green = "#9aae6b"
-local sienna_orange = "#cf6a4c"
-local morningglory_blue = "#8fbfdc"
-local shipcove_blue = "#8197bf"
-local perano_blue = "#b0d0f0"
-local hoki_blue = "#668799"
-local purple_one = "#b6a6ff"
-local wewak_pink = "#f0a0c0"
+  black_one = "#101010",
+  black_two = "#191919",
+  black_three = "#1c1c1c",
+  white_one = "#aaaaaa",
+  gray_one = "#333333",
+  gray_two = "#606060",
 
-local bad = "#d22b2b"
-local neutral = "#ffa500"
-local good = "#60a840"
+  gold_yellow = "#e1b655",
+  koromiko_yellow = "#ffb964",
+  olive_green = "#9aae6b",
+  sienna_orange = "#cf6a4c",
+  morningglory_blue = "#8fbfdc",
+  shipcove_blue = "#8197bf",
+  perano_blue = "#b0d0f0",
+  hoki_blue = "#668799",
+  purple_one = "#b6a6ff",
+  wewak_pink = "#f0a0c0",
+
+  bad = "#d22b2b",
+  neutral = "#ffa500",
+  good = "#60a840",
+}
+
+local p = M.palette
 
 M.style = function()
   -- Main highlight groups
-  hi("Normal", { fg = fg, bg = bg })
-  hi("Cursor", { fg = bg, bg = fg })
+  hi("Normal", { fg = p.fg, bg = p.bg })
+  hi("Cursor", { fg = p.bg, bg = p.fg })
 
   -- Background things
-  hi("SignColumn", { fg = fg, bg = bg })
+  hi("SignColumn", { fg = p.fg, bg = p.bg })
 
-  hi("ColorColumn", { bg = black_two })
+  hi("ColorColumn", { bg = p.black_two })
   hi("CursorColumn", { link = "ColorColumn" })
   hi("CursorLine", { link = "ColorColumn" })
-  hi("VertSplit", { fg = black_three })
+  hi("VertSplit", { fg = p.black_three })
 
-  hi("StatusLine", { fg = fg, bg = black_two })
-  hi("StatusLineNC", { fg = white_one, bg = black_two })
+  hi("StatusLine", { fg = p.fg, bg = p.black_two })
+  hi("StatusLineNC", { fg = p.white_one, bg = p.black_two })
 
-  hi("Visual", { bg = gray_one })
+  hi("Visual", { bg = p.gray_one })
 
   -- Text
-  hi("Comment", { fg = gray_two })
+  hi("Comment", { fg = p.gray_two })
   hi("LineNr", { link = "Comment" })
+  hi("SpecialKey", { link = "Comment" })
   hi("CursorLineNr", { link = "Normal" })
-  hi("MatchParen", { fg = good })
-  hi("Title", { fg = good })
+  hi("MatchParen", { fg = p.good })
+  hi("Title", { fg = p.good })
 
   hi("Bold", { bold = true })
   hi("Italic", { italic = true })
   hi("Underlined", { underline = true })
 
-  hi("Todo", { fg = olive_green })
+  hi("Todo", { fg = p.olive_green })
 
   -- Menus
-  hi("Pmenu", { fg = fg, bg = black_three })
-  hi("PmenuSel", { fg = bg, bg = gold_yellow })
-  hi("PmenuSbar", { fg = gold_yellow, bg = bg })
-  hi("PmenuThumb", { fg = gray_one, bg = gray_one })
-  hi("Search", { fg = bg, bg = gold_yellow })
+  hi("Pmenu", { fg = p.fg, bg = p.black_three })
+  hi("PmenuSel", { fg = p.bg, bg = p.gold_yellow })
+  hi("PmenuSbar", { fg = p.gold_yellow, bg = p.bg })
+  hi("PmenuThumb", { fg = p.gray_one, bg = p.gray_one })
+  hi("Search", { fg = p.bg, bg = p.gold_yellow })
 
   -- Diff mode highlight groups (used as links as well)
-  hi("DiffAdd", { fg = good })
-  hi("DiffChange", { fg = neutral })
-  hi("DiffDelete", { fg = bad })
+  hi("DiffAdd", { fg = p.good })
+  hi("DiffChange", { fg = p.neutral })
+  hi("DiffDelete", { fg = p.bad })
   hi("DiffText", { italic = true })
 
   -- Bad things
-  hi("Error", { fg = bad })
+  hi("Error", { fg = p.bad })
   hi("ErrorMsg", { link = "Error" })
 
   -- Other
-  hi("Directory", { fg = gold_yellow })
+  hi("Directory", { fg = p.gold_yellow })
 
   -- Preferred groups for syntax highlighting (other groups also refer to these)
-  hi("Constant", { fg = sienna_orange })
+  hi("Constant", { fg = p.sienna_orange })
   hi("Character", { link = "Constant" })
   hi("Number", { link = "Constant" })
   hi("Boolean", { link = "Constant" })
   hi("Float", { link = "Constant" })
 
-  hi("String", { fg = olive_green })
+  hi("String", { fg = p.olive_green })
   hi("StringDelimiter", { link = "String" })
 
-  hi("Identifier", { fg = purple_one })
+  hi("Identifier", { fg = p.purple_one })
 
-  hi("Function", { fg = gold_yellow })
+  hi("Function", { fg = p.gold_yellow })
 
-  hi("Statement", { fg = perano_blue })
+  hi("Statement", { fg = p.perano_blue })
   hi("Conditional", { link = "Statement" })
   hi("Repeat", { link = "Statement" })
   hi("Label", { link = "Statement" })
+  hi("Keyword", { link = "Statement" })
 
-  hi("Operator", { fg = morningglory_blue })
+  hi("Operator", { fg = p.morningglory_blue })
 
-  hi("PreProc", { fg = gold_yellow })
+  hi("PreProc", { fg = p.gold_yellow })
   hi("Include", { link = "PreProc" })
   hi("Macro", { link = "PreProc" })
   hi("PreCondit", { link = "PreProc" })
 
-  hi("Type", { fg = koromiko_yellow })
+  hi("Type", { fg = p.koromiko_yellow })
   hi("StorageClass", { link = "Type" })
   hi("Typedef", { link = "Type" })
 
-  hi("Structure", { fg = shipcove_blue })
+  hi("Structure", { fg = p.shipcove_blue })
 
-  hi("Delimiter", { fg = hoki_blue })
+  hi("Delimiter", { fg = p.hoki_blue })
 
   -- Plugin settings
   -- GitSigns
-  hi("GitSignsAdd", { fg = good })
-  hi("GitSignsChange", { fg = neutral })
-  hi("GitSignsDelete", { fg = bad })
+  hi("GitSignsAdd", { fg = p.good })
+  hi("GitSignsChange", { fg = p.neutral })
+  hi("GitSignsDelete", { fg = p.bad })
+
   -- Fidget
   hi("FidgetTask", { link = "Comment" })
-  hi("FidgetTitle", { fg = gold_yellow })
+  hi("FidgetTitle", { fg = p.gold_yellow })
 
   -- Treesitter
-  hi("TSNamespace", { fg = wewak_pink })
-  hi("TSVariable", { link = "Normal" })
-  hi("TSDelimiter", { fg = hoki_blue })
+  hi("TSNamespace", { fg = p.wewak_pink })
+  hi("TSVariable", { link = "Identifier" }) -- Normal
+  hi("TSDelimiter", { fg = p.hoki_blue })
   hi("TSEmphasis", { italic = true })
   hi("TSUnderline", { underline = true })
   hi("TSStrike", { strikethrough = true })
-  hi("TSURI", { fg = morningglory_blue, underline = true })
+  hi("TSURI", { fg = p.morningglory_blue, underline = true })
 
   -- Lualine
-  hi("lualine_a_visual", { bg = morningglory_blue })
+  hi("lualine_a_visual", { bg = p.morningglory_blue })
 
   -- Telescope
-  hi("TelescopeBorder", { fg = black_three })
-  hi("TelescopePromptTitle", { fg = fg })
+  hi("TelescopeBorder", { link = "VertSplit" })
+  hi("TelescopePromptTitle", { fg = p.fg })
   hi("TelescopePromptCounter", { link = "Comment" })
   hi("TelescopePreviewTitle", { link = "TelescopePromptTitle" })
   hi("TelescopeResultsTitle", { link = "TelescopePromptTitle" })
-  hi("TelescopeSelection", { fg = gold_yellow })
+  hi("TelescopeSelection", { fg = p.gold_yellow })
 
   -- Nvim-cmp
-  hi("CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
-  hi("CmpItemAbbrMatch", { fg = gold_yellow, bg = "NONE", bold = true })
-  hi("CmpItemAbbrMatchFuzzy", { fg = gold_yellow, bg = "NONE", bold = true })
+  hi("CmpItemAbbrDeprecated", { fg = p.gray_two, bg = "NONE", strikethrough = true })
+  hi("CmpItemAbbrMatch", { fg = p.gold_yellow, bg = "NONE", bold = true })
+  hi("CmpItemAbbrMatchFuzzy", { fg = p.gold_yellow, bg = "NONE", bold = true })
 
-  hi("CmpItemKindField", { fg = "#c5b5ee" })
-  hi("CmpItemKindProperty", { fg = "#c5b5ee" })
-  hi("CmpItemKindEvent", { fg = "#c5b5ee" })
-  hi("CmpItemKindVariable", { fg = "#c5b5ee" })
+  hi("CmpItemKindVariable", { link = "Identifier" })
+  hi("CmpItemKindField", { link = "CmpItemKindVariable" })
+  hi("CmpItemKindProperty", { link = "CmpItemKindVariable" })
+  hi("CmpItemKindEvent", { link = "CmpItemKindVariable" })
 
-  hi("CmpItemKindText", { fg = "#b2d1f0" })
-  hi("CmpItemKindEnum", { fg = "#b2d1f0" })
-  hi("CmpItemKindKeyword", { fg = "#b2d1f0" })
+  hi("CmpItemKindKeyword", { link = "Keyword" })
+  hi("CmpItemKindText", { link = "CmpItemKindText" })
+  hi("CmpItemKindEnum", { link = "CmpItemKindText" })
 
-  hi("CmpItemKindConstant", { fg = sienna_orange })
-  hi("CmpItemKindConstructor", { fg = sienna_orange })
-  hi("CmpItemKindReference", { fg = sienna_orange })
-  hi("CmpItemKindEnumMember", { fg = sienna_orange })
+  hi("CmpItemKindConstant", { link = "Constant" })
+  hi("CmpItemKindConstructor", { link = "CmpItemKindConstant" })
+  hi("CmpItemKindReference", { link = "CmpItemKindConstant" })
+  hi("CmpItemKindEnumMember", { link = "CmpItemKindConstant" })
 
-  hi("CmpItemKindStruct", { fg = shipcove_blue })
-  hi("CmpItemKindClass", { fg = shipcove_blue })
-  hi("CmpItemKindModule", { fg = shipcove_blue })
-  hi("CmpItemKindInterface", { fg = shipcove_blue })
-  hi("CmpItemKindColor", { fg = shipcove_blue })
+  hi("CmpItemKindStruct", { link = "Structure" })
+  hi("CmpItemKindClass", { link = "Structure" })
+  hi("CmpItemKindModule", { link = "Structure" })
+  hi("CmpItemKindInterface", { link = "Structure" })
+  hi("CmpItemKindColor", { link = "Structure" })
 
-  hi("CmpItemKindOperator", { fg = morningglory_blue })
+  hi("CmpItemKindOperator", { link = "Operator" })
 
-  hi("CmpItemKindUnit", { fg = olive_green })
-  hi("CmpItemKindSnippet", { fg = olive_green })
+  hi("CmpItemKindUnit", { link = "String" })
+  hi("CmpItemKindSnippet", { link = "CmpItemKindUnit" })
 
-  hi("CmpItemKindFile", { fg = gold_yellow })
-  hi("CmpItemKindFolder", { fg = gold_yellow })
+  hi("CmpItemKindFile", { link = "Directory" })
+  hi("CmpItemKindFolder", { link = "Directory" })
 
-  hi("CmpItemKindFunction", { fg = gold_yellow })
-  hi("CmpItemKindTypeParameter", { fg = gold_yellow })
-  hi("CmpItemKindMethod", { fg = gold_yellow })
-  hi("CmpItemKindValue", { fg = gold_yellow })
+  hi("CmpItemKindFunction", { link = "Function" })
+  hi("CmpItemKindTypeParameter", { link = "CmpItemKindFunction" })
+  hi("CmpItemKindMethod", { link = "CmpItemKindFunction" })
+  hi("CmpItemKindValue", { link = "CmpItemKindFunction" })
 
   -- Indent-blankline
-  hi("IndentBlanklineChar", { fg = gray_one, nocombine = true })
+  hi("IndentBlanklineChar", { fg = p.gray_one, nocombine = true })
 
   -- Nvimtree
-  hi("NvimTreeWindowPicker", { fg = gold_yellow, bg = black_two })
-  hi("NvimTreeWinSeparator", { fg = black_one, bg = black_one })
-  hi("NvimTreeNormal", { bg = black_one})
+  hi("NvimTreeWindowPicker", { fg = p.gold_yellow, bg = p.black_two })
+  hi("NvimTreeWinSeparator", { fg = p.black_one, bg = p.black_one })
+  hi("NvimTreeNormal", { bg = p.black_one })
 end
 
 return M
