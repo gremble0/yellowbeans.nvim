2691f3d90d595f7bcaec67ae2a0dfc5ed3b7ed7b
gremble0 <hermanstornes@gmail.com>
2023-07-27 17:17:12 +0200

    Moved highlight groups into the styles module

--- lua/yellowbeans/styles.lua
+++ lua/yellowbeans/styles.lua
@@ -1,11 +1,12 @@
 --[[
 This file defines styles that can be `require()`d by other programs also 
-outside of the theme itself. For example I would recommend using the border
-field in your configs of other plugins so that the background colors dont 
-blend in weirdly.
+outside of the theme itself. For example you could use the M.border field
+so in plugins that use floating windows
 --]]
+
 local M = {}
 
+-- Colorcodes to be used for highlight groups
 M.palette = {
   bg = "#151515",
   fg = "#cccccc",
@@ -33,6 +34,9 @@ M.palette = {
   good = "#60a840",
 }
 
+-- Table of tables containing special symbols used as a border with the
+-- corresponding highlight group used for this symbol. This format works for
+-- lspconfig.ui.windows
 M.border = {
   { "┌", "VertSplit" },
   { "─", "VertSplit" },
@@ -43,5 +47,183 @@ M.border = {
   { "└", "VertSplit" },
   { "│", "VertSplit" }
 }
+
+-- Defined abbrevation for ease of typing
+local p = M.palette
+
+M.hl_groups = {
+  -- Main highlight groups
+  { "Normal", { fg = p.fg, bg = p.bg } },
+  { "Conceal", { link = "Normal" } },
+  { "NonText", { fg = p.fg } },
+
+  { "Cursor", { fg = p.bg, bg = p.fg } },
+  { "TermCursor", { link = "Cursor" } },
+
+  -- Background things
+  { "SignColumn", { fg = p.fg, bg = p.bg } },
+
+  { "ColorColumn", { bg = p.black_two } },
+  { "CursorColumn", { link = "ColorColumn" } },
+  { "CursorLine", { link = "ColorColumn" } },
+  { "VertSplit", { fg = p.black_three } },
+
+  { "StatusLine", { link = "ColorColumn" } },
+  { "StatusLineNC", { fg = p.white_one, bg = p.black_two } },
+
+  { "Visual", { bg = p.gray_one } },
+
+  -- Text
+  { "Comment", { fg = p.gray_two } },
+  { "LineNr", { link = "Comment" } },
+  { "Special", { fg = p.shipcove_blue } },
+  { "SpecialKey", { link = "Comment" } },
+  { "CursorLineNr", { link = "Normal" } },
+  { "MatchParen", { fg = p.gold_yellow } },
+  { "Title", { fg = p.good } },
+
+  { "Bold", { bold = true } },
+  { "Italic", { italic = true } },
+  { "Underlined", { underline = true } },
+
+  { "Todo", { fg = p.olive_green } },
+
+  -- Menus
+  { "Pmenu", { fg = p.fg, bg = p.bg } },
+  { "PmenuBg", { link = "VertSplit" } },
+  { "PmenuSel", { fg = p.bg, bg = p.gold_yellow } },
+  { "PmenuSbar", { fg = p.gold_yellow, bg = p.bg } },
+  { "PmenuThumb", { fg = p.gray_one, bg = p.gray_one } },
+
+  { "Search", { fg = p.bg, bg = p.gold_yellow } },
+  { "IncSearch", { link = "Search" } },
+
+  -- Diff mode highlight groups (used as links as well },
+  { "DiffAdd", { fg = p.good } },
+  { "DiffChange", { fg = p.neutral } },
+  { "DiffDelete", { fg = p.bad } },
+  { "DiffText", { italic = true } },
+
+  -- Bad things
+  { "Error", { fg = p.bad } },
+  { "ErrorMsg", { link = "Error" } },
+
+  -- Other
+  { "Directory", { fg = p.gold_yellow } },
+
+  -- Preferred groups for syntax highlighting (other groups also refer to these },
+  { "Constant", { fg = p.sienna_orange } },
+  { "Character", { link = "Constant" } },
+  { "Number", { link = "Constant" } },
+  { "Boolean", { link = "Constant" } },
+  { "Float", { link = "Constant" } },
+
+  { "String", { fg = p.olive_green } },
+  { "StringDelimiter", { link = "String" } },
+
+  { "Identifier", { fg = p.purple_one } },
+
+  { "Function", { fg = p.gold_yellow } },
+
+  { "Statement", { fg = p.perano_blue } },
+  { "Conditional", { link = "Statement" } },
+  { "Repeat", { link = "Statement" } },
+  { "Label", { link = "Statement" } },
+  { "Keyword", { link = "Statement" } },
+
+  { "Operator", { fg = p.morningglory_blue } },
+
+  { "PreProc", { fg = p.gold_yellow } },
+  { "Include", { link = "PreProc" } },
+  { "Macro", { link = "PreProc" } },
+  { "PreCondit", { link = "PreProc" } },
+
+  { "Type", { fg = p.koromiko_yellow } },
+  { "StorageClass", { link = "Type" } },
+  { "Typedef", { link = "Type" } },
+
+  { "Structure", { fg = p.shipcove_blue } },
+
+  { "Delimiter", { fg = p.hoki_blue } },
+
+  --[[ Plugin settings ]]
+  -- GitSigns
+  { "GitSignsAdd", { fg = p.good } },
+  { "GitSignsChange", { fg = p.neutral } },
+  { "GitSignsDelete", { fg = p.bad } },
+
+  -- Fidget
+  { "FidgetTask", { link = "Comment" } },
+  { "FidgetTitle", { fg = p.gold_yellow } },
+
+  -- Treesitter
+  { "TSNamespace", { fg = p.wewak_pink } },
+  { "TSDelimiter", { fg = p.hoki_blue } },
+
+  { "TSStringRegex", { fg = p.sienna_orange } },
+  { "TSStringEscape", { fg = p.sienna_orange } },
+
+  { "TSURI", { fg = p.morningglory_blue, underline = true } },
+  { "TSEmphasis", { italic = true } },
+  { "TSUnderline", { underline = true } },
+  { "TSStrike", { strikethrough = true } },
+
+  { "TSError", { link = "Error" } },
+
+  -- Telescope
+  { "TelescopeBorder", { link = "VertSplit" } },
+  { "TelescopePromptTitle", { fg = p.fg } },
+  { "TelescopePromptCounter", { link = "Comment" } },
+  { "TelescopePreviewTitle", { link = "TelescopePromptTitle" } },
+  { "TelescopeResultsTitle", { link = "TelescopePromptTitle" } },
+  { "TelescopeSelection", { fg = p.gold_yellow } },
+
+  -- Nvim-cmp
+  { "CmpItemAbbrDeprecated", { fg = p.gray_two, bg = "NONE", strikethrough = true } },
+  { "CmpItemAbbrMatch", { fg = p.gold_yellow, bg = "NONE", bold = true } },
+  { "CmpItemAbbrMatchFuzzy", { fg = p.gold_yellow, bg = "NONE", bold = true } },
+
+  { "CmpItemKindVariable", { link = "Identifier" } },
+  { "CmpItemKindField", { link = "CmpItemKindVariable" } },
+  { "CmpItemKindProperty", { link = "CmpItemKindVariable" } },
+  { "CmpItemKindEvent", { link = "CmpItemKindVariable" } },
+
+  { "CmpItemKindKeyword", { link = "Keyword" } },
+  { "CmpItemKindText", { link = "CmpItemKindText" } },
+  { "CmpItemKindEnum", { link = "CmpItemKindText" } },
+
+  { "CmpItemKindConstant", { link = "Constant" } },
+  { "CmpItemKindConstructor", { link = "CmpItemKindConstant" } },
+  { "CmpItemKindReference", { link = "CmpItemKindConstant" } },
+  { "CmpItemKindEnumMember", { link = "CmpItemKindConstant" } },
+
+  { "CmpItemKindStruct", { link = "Structure" } },
+  { "CmpItemKindClass", { link = "CmpItemKindStruct" } },
+  { "CmpItemKindModule", { link = "CmpItemKindStruct" } },
+  { "CmpItemKindInterface", { link = "CmpItemKindStruct" } },
+  { "CmpItemKindColor", { link = "CmpItemKindStruct" } },
+
+  { "CmpItemKindOperator", { link = "Operator" } },
+
+  { "CmpItemKindUnit", { link = "String" } },
+  { "CmpItemKindSnippet", { link = "CmpItemKindUnit" } },
+
+  { "CmpItemKindFolder", { link = "Directory" } },
+  { "CmpItemKindFile", { link = "CmpItemKindFolder" } },
+
+  { "CmpItemKindFunction", { link = "Function" } },
+  { "CmpItemKindTypeParameter", { link = "CmpItemKindFunction" } },
+  { "CmpItemKindMethod", { link = "CmpItemKindFunction" } },
+  { "CmpItemKindValue", { link = "CmpItemKindFunction" } },
+
+  -- Indent-blankline
+  { "IndentBlanklineChar", { fg = p.black_three, nocombine = true } },
+
+  -- Nvimtree
+  { "NvimTreeWindowPicker", { fg = p.gold_yellow, bg = p.black_two } },
+  { "NvimTreeWinSeparator", { fg = p.black_one, bg = p.black_one } },
+  { "NvimTreeNormal", { bg = p.black_one } },
+}
+
 
 return M
